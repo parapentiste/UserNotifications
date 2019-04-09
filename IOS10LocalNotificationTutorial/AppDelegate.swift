d937acc5e1b6ae3abe76064c918c4a823dccafde
Sabrina LIN <sabrina@sabrinas-MacBook-Pro.local>
2019-04-09 10:55:37 +0800

    experiment with multiple notifications instead of showing one repeatedly.

--- IOS10LocalNotificationTutorial/AppDelegate.swift
+++ IOS10LocalNotificationTutorial/AppDelegate.swift
@@ -13,10 +13,49 @@ import UserNotifications
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+//    func scheduleNotifications () {
+//        // 1 The UNMutableNOtificationContent object contains the data of the notification
+//        let content = UNMutableNotificationContent()
+//        content.title = "Notification Tutorial"
+//        content.subtitle = "from ioscreator.com"
+//        content.body = " Notification triggered"
+//        //content.badge = 2
+//        //content.sound = UNNotificationSound.default()
+//
+//        // 2 The UNNotificationAttachment object contains the media content of the notification
+//        let imageName = "applelogo"
+//        guard let imageURL = Bundle.main.url(forResource: imageName, withExtension: "png") else { return }
+//
+//        let attachment = try! UNNotificationAttachment(identifier: imageName, url: imageURL, options: .none)
+//
+//        content.attachments = [attachment]
+//
+//        // 3 create a trigger; specify the condition of delivery
+//        // case 1: non-repeat trigger
+//        //   let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+//        // case 2: repeat trigger
+//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 60, repeats: true)
+//
+//        // 4 create a request with the trigger and the content
+//        let request = UNNotificationRequest(identifier: "notification.id.01", content: content, trigger: trigger)
+//
+//        // 5 schedule the request with the system
+//        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+//    }
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
+            if success {
+                print("success")
+            } else {
+                print("error")
+            }
+        }
+     //   scheduleNotifications()
+    
         return true
     }
 
@@ -40,10 +79,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        print("\(URL(fileURLWithPath: #file).lastPathComponent,#function,#line)- ")
-//    UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: ["notification.id.01"])
-//        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
         
+//    UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: ["notification.id.01"])
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        // ^ above code does not work. Why?
+        print("\(URL(fileURLWithPath: #file).lastPathComponent,#function,#line)- ")
         
     }
 
--- IOS10LocalNotificationTutorial/ViewController.swift
+++ IOS10LocalNotificationTutorial/ViewController.swift
@@ -12,8 +12,8 @@ import UserNotifications
 class ViewController: UIViewController {
 
     @IBAction func removeNotification(_ sender: Any) {
-      //  UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
-        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: ["notification.id.01"])
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+      //  UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: ["notification.id.01"])
         // ^ originally added above function to AppDelegate's "applicationWillTerminate()" but was not called. Why ???
         
     }
@@ -25,26 +25,37 @@ class ViewController: UIViewController {
         content.body = " Notification triggered"
         //content.badge = 2
         //content.sound = UNNotificationSound.default()
-        
+
         // 2 The UNNotificationAttachment object contains the media content of the notification
         let imageName = "applelogo"
         guard let imageURL = Bundle.main.url(forResource: imageName, withExtension: "png") else { return }
-        
+
         let attachment = try! UNNotificationAttachment(identifier: imageName, url: imageURL, options: .none)
-        
+
         content.attachments = [attachment]
-        
+
         // 3 create a trigger; specify the condition of delivery
         // case 1: non-repeat trigger
-     //   let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let trigger1 = UNTimeIntervalNotificationTrigger(timeInterval: 15, repeats: false)
+        let trigger2 = UNTimeIntervalNotificationTrigger(timeInterval: 25, repeats: false)
+        let trigger3 = UNTimeIntervalNotificationTrigger(timeInterval: 35, repeats: false)
+        let trigger4 = UNTimeIntervalNotificationTrigger(timeInterval: 45, repeats: false)
         // case 2: repeat trigger
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 60, repeats: true)
-        
+     //   let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 60, repeats: true)
+
         // 4 create a request with the trigger and the content
         let request = UNNotificationRequest(identifier: "notification.id.01", content: content, trigger: trigger)
-        
+        let request1 = UNNotificationRequest(identifier: "notification.id.02", content: content, trigger: trigger1)
+        let request2 = UNNotificationRequest(identifier: "notification.id.03", content: content, trigger: trigger2)
+        let request3 = UNNotificationRequest(identifier: "notification.id.04", content: content, trigger: trigger3)
+        let request4 = UNNotificationRequest(identifier: "notification.id.05", content: content, trigger: trigger4)
         // 5 schedule the request with the system
         UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        UNUserNotificationCenter.current().add(request1, withCompletionHandler: nil)
+        UNUserNotificationCenter.current().add(request2, withCompletionHandler: nil)
+        UNUserNotificationCenter.current().add(request3, withCompletionHandler: nil)
+        UNUserNotificationCenter.current().add(request4, withCompletionHandler: nil)
     }
     
     override func viewDidLoad() {
@@ -53,13 +64,13 @@ class ViewController: UIViewController {
 //        var myTimer = Timer()
 //        myTimer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: <#T##Selector#>, userInfo: nil, repeats: true)
         
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
-                if success {
-                    print("success")
-                } else {
-                    print("error")
-                }
-            }
+//        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
+//                if success {
+//                    print("success")
+//                } else {
+//                    print("error")
+//                }
+//            }
     }
     
     
